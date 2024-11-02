7903b306463432dccba4516b9d9a83846819833f
YungzKremz <yungzkremz1@gmail.com>
2024-11-02 10:47:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script for adding interactivity
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Message Sent Successfully!');
+});
+
+
